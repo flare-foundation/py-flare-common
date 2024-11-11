18a0b1365d7e69983b572d542370b34796082faa
andraz maier <andraz.maier@aflabs.com>
2024-11-11 09:27:28 +0100

    feat: add bytes parsing for FTSO

--- src/py_flare_common/merkle/hexstr.py
+++ src/py_flare_common/merkle/hexstr.py
@@ -12,3 +12,11 @@ def prefix_0x(s: str) -> str:
 def is_hex_str(s: str):
     s = un_prefix_0x(s)
     return all(c in string.hexdigits for c in s)
+
+
+def to_bytes(s: str | bytes) -> bytes:
+    if isinstance(s, str):
+        if not is_hex_str(s):
+            raise ValueError("Invalid hex string")
+        s = bytes.fromhex(s)
+    return s
