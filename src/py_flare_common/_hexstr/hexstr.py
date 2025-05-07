4605e34da04c09e5bee20e326941d38277a37f78
Matej Janežič <janezic.mj@gmail.com>
2025-05-07 14:55:09 +0200

    feat: strip function selector in parse

--- src/py_flare_common/_hexstr/hexstr.py
+++ src/py_flare_common/_hexstr/hexstr.py
@@ -16,9 +16,26 @@ def is_hex_str(s: str):
     return all(c in string.hexdigits for c in s)
 
 
+SUBMISSION_METHOD_SELECTORS = {
+    b"\x6c\x53\x2f\xae",  # submit1
+    b"\x9d\x00\xc9\xfd",  # submit2
+    b"\xe1\xb1\x57\xe7",  # submit3
+    b"\x57\xee\xd5\x80",  # submitSignatures
+}
+
+
 def to_bytes(s: str | bytes) -> bytes:
+    # cleanup string and convert to bytes
     if isinstance(s, str):
+        s = un_prefix_0x(s)
+
         if not is_hex_str(s):
             raise ValueError("Invalid hex string")
+
         s = bytes.fromhex(s)
+
+    # "magic" detection of function signature
+    if s[0:4] in SUBMISSION_METHOD_SELECTORS:
+        s = s[4:]
+
     return s
